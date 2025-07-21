0a3f33836190dbb8886561222e3ad740d84e8fe6
david-ouatedem <davdev1400@gmail.com>
2025-07-21 18:48:53 +0100

    first commit

---
+++ 01-starting-project/src/app/user/user.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, input, Input, Output } from '@angular/core';
+import { User } from '../dummy-users';
+import { CardComponent } from "../shared/card/card.component";
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [CardComponent],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css'
+})
+export class UserComponent {
+  @Input({ required: true }) user!: User;
+  @Input({ required: true }) isSelected!: boolean;
+  @Output() select = new EventEmitter<string>();
+
+  // avatar = input.required<string>()
+  // name = input.required<string>()
+
+  get imagePath() {
+    return `assets/users/${this.user.avatar}`
+  }
+
+  onSelectUser() {
+    this.select.emit(this.user.id);
+  }
+}
